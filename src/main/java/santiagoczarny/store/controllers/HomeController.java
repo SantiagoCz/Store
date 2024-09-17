d3a226e17449164513d5c1a86ac86775d17f925d
SantiagoCz <czarnysantiago@gmail.com>
2024-09-17 20:25:19 -0300

    Add @Controller annotation to HomeController for handling web requests

--- src/main/java/santiagoczarny/store/controllers/HomeController.java
+++ src/main/java/santiagoczarny/store/controllers/HomeController.java
@@ -1,8 +1,10 @@
 package santiagoczarny.store.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+@RestController
 public class HomeController {
 
     @GetMapping(value = { "/", "/home", "/inicio", "/index" })
