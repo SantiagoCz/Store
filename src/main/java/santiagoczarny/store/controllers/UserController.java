264efa8e6b3492832658c4efe50f71049cb3275d
SantiagoCz <134440621+SantiagoCz@users.noreply.github.com>
2024-09-17 21:47:52 -0300

    Add newUser method with ModelAndView for user registration form

--- src/main/java/santiagoczarny/store/controllers/UserController.java
+++ src/main/java/santiagoczarny/store/controllers/UserController.java
@@ -1,8 +1,10 @@
 package santiagoczarny.store.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import santiagoczarny.store.services.PersonService;
 import santiagoczarny.store.services.UserService;
 
@@ -14,5 +16,16 @@ public class UserController {
     private UserService userService;
     @Autowired
     private PersonService personService;
+
+    @GetMapping(value = {"/new"})
+    public ModelAndView newUser() {
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("fragments/base");
+        mav.addObject("title", "Registrar Usuario");
+        mav.addObject("view", "createForms/user-form");
+        mav.addObject("includeScript", true);
+        return mav;
+    }
 
 }
